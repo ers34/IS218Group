c312e6961965fec292a5a998cb3a51edccc4388b
eileenrsanchez <ers34@njit.edu>
2020-12-15 00:32:47 -0500

    Initial Commit

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+define('sql_server', 'sql1.njit.edu');
+define('sql_username', 'ers34');
+define('sql_pass', '!Sunflower99');
+define('db_name', 'ers34');
+
+/* Attempt to connect to MySQL database */
+try {
+    $conn = new PDO("mysql:host=" . sql_server . ";dbname=" . db_name, sql_username, sql_passl);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+?>
